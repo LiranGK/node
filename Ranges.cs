b63fd48bd5589530ad3ddd7e9a1cfb317ed1e63d
LiranGK <lirangilday@gmail.com>
2025-04-03 14:01:11 +0300

    Add files via upload

---
+++ Ranges.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagrutListLab
+{
+    public class Ranges
+    {
+        private int high;
+        private int low;
+        public Ranges(int high, int low)
+        {
+            this.high = high;
+            this.low = low;
+        }
+        public int GetHigh()
+        {
+            return this.high;
+        }
+        public int GetLow()
+        {
+            return this.low;
+        }
+        public override string ToString()
+        {
+            string theString = "[low: "+low+", high: "+high+"]";
+            return theString;
+        }
+    }
+}
